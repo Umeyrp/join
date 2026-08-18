00ded81c56cc884082c81673ff9035371a470ead
Mathias Mayer <mayer.mathias@proton.me>
2026-08-18 13:04:35 +0200

    add: dropdown menu on desktop

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  imports: [RouterLink],
-  templateUrl: './header.html',
-  styleUrl: './header.scss',
+    selector: 'app-header',
+    imports: [RouterLink],
+    templateUrl: './header.html',
+    styleUrl: './header.scss',
 })
 export class Header {
-  isLoggedIn = true;
-  isMenuOpen = false;
+    isLoggedIn = true;
+    isMenuOpen = signal(false);
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
+    toggleMenu() {
+        this.isMenuOpen.update((v) => !v);
+    }
 }
