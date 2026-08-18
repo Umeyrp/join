752425a06fe57ed5583162df4156e2bfcc520401
Mathias Mayer <mayer.mathias@proton.me>
2026-08-18 12:26:57 +0200

    fix: routing contact details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,18 @@ import { Contacts } from './pages/main/contacts/contacts';
 import { ContactDetails } from './pages/main/contacts/contact-details/contact-details';
 
 export const routes: Routes = [
-  { path: 'login', component: Login },
-  {
-    path: '',
-    component: Main,
-    children: [
-      { path: 'contacts', component: Contacts },
-      { path: 'summary', component: Summary },
-      { path: 'contacts', component: Contacts },
-      { path: '', redirectTo: 'summary', pathMatch: 'full' },
-    ],
-  },
+    { path: 'login', component: Login },
+    {
+        path: '',
+        component: Main,
+        children: [
+            { path: 'summary', component: Summary },
+            {
+                path: 'contacts',
+                component: Contacts,
+                children: [{ path: ':id', component: ContactDetails }],
+            },
+            { path: '', redirectTo: 'summary', pathMatch: 'full' },
+        ],
+    },
 ];
