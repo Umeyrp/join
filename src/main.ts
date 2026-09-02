ef65330f8dab877c3c3fd724271d87075c49ca5c
Mathias Mayer <mayer.mathias@proton.me>
2026-09-02 11:35:43 +0200

    fix: console-log-error on certain conditions .

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+const originalOnError = window.onerror;
+window.onerror = (message, source, lineno, colno, error) => {
+    if (error instanceof TypeError && error.message.includes('startTime')) {
+        return true;
+    }
+    return originalOnError ? originalOnError(message, source, lineno, colno, error) : false;
+};
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
