53cf01cc7e0a02af6c53c54255bb04c4f65cc086
Mathias Mayer <mayer.mathias@proton.me>
2026-09-09 10:12:39 +0200

    add: privacy policy

--- src/app/pages/main/privacy-policy/privacy-policy.ts
+++ src/app/pages/main/privacy-policy/privacy-policy.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
     selector: 'app-privacy-policy',
@@ -6,4 +7,10 @@ import { Component } from '@angular/core';
     templateUrl: './privacy-policy.html',
     styleUrl: './privacy-policy.scss',
 })
-export class PrivacyPolicy {}
+export class PrivacyPolicy {
+    private location = inject(Location);
+
+    goBack() {
+        this.location.back();
+    }
+}
